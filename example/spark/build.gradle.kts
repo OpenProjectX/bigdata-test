c46db5833141fe04e73d8fe903850347b36e8b2d
CoderYellow <CoderYellow@hotmail.com>
2026-06-01 22:12:04 +0800

    chore: bump `org.openprojectx.spark.platform` to `0.1.39` in Spark example

--- example/spark/build.gradle.kts
+++ example/spark/build.gradle.kts
@@ -6,7 +6,7 @@ import org.gradle.api.attributes.Usage
 
 plugins {
     id("buildsrc.convention.kotlin-jvm")
-    id("org.openprojectx.spark.platform") version "0.1.38-SNAPSHOT"
+    id("org.openprojectx.spark.platform") version "0.1.39"
     id("org.openprojectx.hadoop-native-loader") version "0.1.1"
 
 }
